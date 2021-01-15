91041746333d3bf58a5fe3d7a3065263b54ab468
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-01-15 22:41:05 +0100

    Replace array with DiceHand in yatzy method

--- src/main/java/yatzi/YatzyTest.java
+++ src/main/java/yatzi/YatzyTest.java
@@ -9,16 +9,17 @@ public class YatzyTest {
     public void chance_scores_sum_of_all_dice() {
         int expected = 15;
         int actual = Yatzy.chance(2,3,4,5,1);
+
         assertEquals(expected, actual);
         assertEquals(16, Yatzy.chance(3,3,4,5,1));
     }
 
     @Test public void yatzy_scores_50() {
         int expected = 50;
-        int actual = Yatzy.yatzy(4,4,4,4,4);
+        int actual = Yatzy.yatzy(new DiceHand(4,4,4,4,4));
         assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
-        assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
+        assertEquals(50, Yatzy.yatzy(new DiceHand(6,6,6,6,6)));
+        assertEquals(0, Yatzy.yatzy(new DiceHand(6,6,6,6,3)));
     }
 
     @Test public void test_1s() {
