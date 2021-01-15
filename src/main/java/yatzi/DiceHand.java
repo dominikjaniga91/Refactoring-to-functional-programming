99086855ecdc921be7af97d125ce48c45060d441
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-01-15 21:15:26 +0100

    Create a dice hand class

---
+++ src/main/java/yatzi/DiceHand.java
@@ -0,0 +1,10 @@
+package yatzi;
+
+public class DiceHand {
+
+    private final int[] dice;
+
+    public DiceHand(int d1, int d2, int d3, int d4, int d5) {
+        this.dice = new int[]{d1, d2, d3, d4, d5};
+    }
+}
